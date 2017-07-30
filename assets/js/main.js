f0d1ee525819364b02dcfe36f5ff255301c52615
vanita22 <inesvanessapcm@gmail.com>
2017-07-30 00:44:03 -0400

    cargando menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){ 
 
-	/*SE HACE EL RECORRIDO DE CADA ELEMENTO DENTRO DEL OBJETO Y SE AGREGA EN SUS RESPECTIVAS CAJAS*/
+	/*HACE EL RECORRIDO DE CADA ELEMENTO DENTRO DEL OBJETO Y SE AGREGA EN SUS RESPECTIVAS CAJAS*/
 	for(i=0; i<=3; i++){
 		$(".box").append("<img src=dist/img/"+ dat[i].image_url + ">" + "<h4>" + dat[i].title + "</h4>" + "<h5>" + dat[i].description + "</h5>" + "<div><h3>M</h3><p>"+dat[i].user+"</p><h2>#"+dat[i].hashtag+"</h2></div>" + "<br>");
 	}
@@ -20,6 +20,18 @@ $(document).ready(function(){
 	for(i=16; i<=19; i++){
 		$(".box5").append("<img src=dist/img/"+ dat[i].image_url + ">" + "<h4>" + dat[i].title + "</h4>" + "<h5>" + dat[i].description + "</h5>" + "<div><h3>M</h3><p>"+dat[i].user+"</p><h2>#"+dat[i].hashtag+"</h2></div>" + "<br>");
 	}
+
+	/*MUESTRA Y OCULTA EL SUB-MENU*/
+
+	$(".lista").hide();
+	
+	$(".hambur").click(function(){
+		$(".lista").show(300);
+	})
+
+	$(".close").click(function(){
+		$(".lista").hide(300);
+	})
 })
 
 
